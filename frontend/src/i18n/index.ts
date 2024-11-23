464aca5ac76de021d8d8d3fd3317ef56defb0029
Aristote-code <gahimaaristote1@gmail.com>
2024-11-23 12:21:04 +0200

    Updated multilingual-file-manager to version 1.0.0

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -3,6 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json';
 import es from './locales/es.json';
+import fr from './locales/fr.json';
+import sw from './locales/sw.json';
+import rw from './locales/rw.json';
 
 i18n
   .use(LanguageDetector)
@@ -10,8 +13,12 @@ i18n
   .init({
     resources: {
       en: { translation: en },
-      es: { translation: es }
+      es: { translation: es },
+      fr: { translation: fr },
+      sw: { translation: sw },
+      rw: { translation: rw }
     },
+    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
